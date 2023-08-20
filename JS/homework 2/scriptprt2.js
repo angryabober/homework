2343830b670e632ed7512932c24c637ae89e2b68
angryabober <pvrfury@gmail.com>
2023-08-20 16:07:10 +0300

    f

---
+++ JS/homework 2/scriptprt2.js
@@ -0,0 +1,8 @@
+let welcomeMessage = "Добро пожаловать,администратор!"
+let username = prompt('кто вы?')
+
+if (username == 'admin') {
+    alert('Добро пожаловать,администратор!')
+}else{
+    alert('Доступ запрещён!')
+}
