b3573171c6ff941306aef73504e928582691b933
angryabober <pvrfury@gmail.com>
2023-08-20 15:52:04 +0300

    f

--- JS/homework 2/scripptt4.js
+++ JS/homework 2/scripptt4.js
@@ -2,5 +2,13 @@ let student = prompt('вы студент? (1-да/2-нет)')
 let job = prompt('вы работаете? (1-да/2-нет)')
 
 if (student == 1 && job == 1){
-    alert('bobr aasdsfdfswd')
+    alert('вы студент и вы работаете');
+}
+
+if (student == 1 && job == 2){
+    alert('вы студент');
+}
+
+if (student == 2 && job == 1){
+    alert('вы работаете');
 }
