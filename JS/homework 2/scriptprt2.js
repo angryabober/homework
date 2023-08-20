35543e5294c2cb840bbf6d4a2e75523260f4ee05
angryabober <pvrfury@gmail.com>
2023-08-20 14:31:03 +0300

    f

--- JS/homework 2/scriptprt2.js
+++ JS/homework 2/scriptprt2.js
@@ -1,10 +1,10 @@
-let num = prompt('введите число')
-let answr = (num % 2)
+let num = 7
+let answr = (num % 2);
 
-if (answr = 0);{
+if (answr = 0); {
     alert('число чётное');
 }
 
-if (answr > 0) {
+if (answr > 0); {
     alert('число не чётное');
 }
