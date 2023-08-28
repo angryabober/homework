5ef02d877e6180d4025c51e9c5e6fdcfcf3c4cc6
angryabober <pvrfury@gmail.com>
2023-08-28 21:06:54 +0300

    f

--- JS/homework 3/prt6.js
+++ JS/homework 3/prt6.js
@@ -2,7 +2,7 @@ let num = prompt('введите трёхзначное число');
 let num1 = (num / 100);
 let num2 = (num / 10 % 10);
 let num3 = (num % 10);
-if (num1==num2 && num2==num3 && num1==num3) {
+if (num1 == num2 && num2 == num3 && num1 == num3) {
     alert('найдено совпадение');
 }
 else {
