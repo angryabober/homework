6ed4e7fee43dd181ba234a74c16ebc0a30a7a54d
angryabober <pvrfury@gmail.com>
2023-08-28 20:34:27 +0300

    f

---
+++ JS/homework 3/prt5.js
@@ -0,0 +1,31 @@
+let num = prompt('введите число от 0 до 9')
+if (num = 0) {
+    alert(')')
+}
+if (num = 1) {
+    alert('!')
+}
+if (num = 2) {
+    alert('@')
+}
+if (num = 3) {
+    alert('#')
+}
+if (num = 4) {
+    alert('$')
+}
+if (num = 5) {
+    alert('%')
+}
+if (num = 6) {
+    alert('^')
+}
+if (num = 7) {
+    alert('&')
+}
+if (num = 8) {
+    alert('*')
+}
+if (num = 9) {
+    alert('(')
+}
