2e281659e574e4a7f9018277983fa0c3855b7e98
angryabober <pvrfury@gmail.com>
2023-08-28 19:07:40 +0300

    а

--- JS/homework 3/prt1.js
+++ JS/homework 3/prt1.js
@@ -1,2 +1,9 @@
 const GBMB = 1024;
-var flashka = Number(prompt('объём флэшки в ГБ'))
+let flashka = Number(prompt('объём флэшки в ГБ'));
+let size = (flashka * 1000 / GBMB);
+if (size < 0) {
+    alert('не хватает места');
+}
+else {
+    alert('места хватит');
+}
