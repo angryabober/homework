f22f4c56ec25f40a70521b9e095f35099133c647
angryabober <pvrfury@gmail.com>
2023-08-29 15:58:35 +0300

    f

---
+++ JS/homework 4/prt1.js
@@ -0,0 +1,8 @@
+let min = 0;
+let max = 3;
+let num = Math.floor(Math.random() * (max - min + 1)) + min;
+let answr = prompt('введите число')
+if (answr == num) {
+    alert('вы угадали')
+}
+else('попробуйте ещё')
