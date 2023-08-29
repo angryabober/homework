4e4504ace1e6165d59b830a1d21b2e36a8dcb1c0
angryabober <pvrfury@gmail.com>
2023-08-29 17:30:17 +0300

    f

--- JS/homework 4/prt2.js
+++ JS/homework 4/prt2.js
@@ -1,8 +1,6 @@
 let answer;
 let all = 0;
 while (answer !== '0') {
-    all = (Number(answer) + Number(all))
     answer = prompt('впишите другое число');
 }
 alert('вы вписали верное число');
-alert(all);
