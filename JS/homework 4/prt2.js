96275613cddbd2fda8e02a5c7bdeb153cdfc1714
angryabober <pvrfury@gmail.com>
2023-08-29 17:21:08 +0300

    f

--- JS/homework 4/prt2.js
+++ JS/homework 4/prt2.js
@@ -1,9 +1,7 @@
 let answer;
 let all = 0;
 while (answer !== '0') {
-    all = Number(all)
-    answer = Number(answer)
-    all = all + answer
+    all = (Number(answer) + Number(all))
     answer = prompt('впишите другое число');
 }
 alert('вы вписали верное число');
