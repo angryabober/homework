c6f91418cdebf49d672dd68164b5648376ae1fed
angryabober <pvrfury@gmail.com>
2023-09-10 20:34:35 +0300

    f

--- JS/homework 7/prt1.js
+++ JS/homework 7/prt1.js
@@ -1,3 +1,15 @@
+function calc(callback){
 let num1 = prompt('введите число 1');
-let symb = prompt('введите символ');
 let num2 = prompt('введите число 2');
+let result = callback(num1,num2);
+}
+
+function umnozh(num1,num2) {
+    return num1 * num2;
+}
+function del(num1,num2) {
+    return num1 / num2;
+}
+function plus(num1,num2) {
+    return num1 + num2;
+}
